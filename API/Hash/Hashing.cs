396361de269fe800920ca44f5b84709d6afcde36
suwardihtg <galung.suwardi@gmail.com>
2022-10-10 01:58:20 +0700

    SetUp App

---
+++ API/Hash/Hashing.cs
@@ -0,0 +1,20 @@
+﻿namespace API.Hash
+{
+    public class Hashing
+    {
+        private static string GetRandomSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(12);
+        }
+
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, GetRandomSalt());
+        }
+
+        public static bool ValidatePassword(string password, string coreectHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, coreectHash);
+        }
+    }
+}
